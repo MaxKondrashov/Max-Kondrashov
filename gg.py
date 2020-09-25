4761c9cfc7963bd8b42170e176ec4f411a69ea1d
Max Kondrashov <71550783+MaxKondrashov@users.noreply.github.com>
2020-09-25 13:10:55 +0300

    Add files via upload

---
+++ gg.py
@@ -0,0 +1,15 @@
+print("Hello world!") #1
+print(3 + 4) #2
+print(3 * 5) #3
+print(3 ** 2) #4
+
+name = input("Как Вас зовут? ") #5
+print("Привет,", name) 
+
+if (a == 1 and b == 2 and c == 3 and d == 4):
+  print('spam' * 3)
+
+x = 12
+y = 7
+if x > y: print(x)
+
